843ed21f7e78a86782c4203f1d4006ae37f0d424
Ross Faulds <ross.faulds@bjss.com>
2019-03-07 13:49:56 +0000

    add x86_64 to supported devices for running in simulator

--- src/functions/getConfiguration/domain/config.mock.ts
+++ src/functions/getConfiguration/domain/config.mock.ts
@@ -2,7 +2,10 @@ import { Config } from './config.model';
 
 export const config : Config = {
   googleAnalyticsId: 'UA-129489007-3',
-  approvedDeviceIdentifiers: ['iPad7,4'],
+  approvedDeviceIdentifiers: [
+    'iPad7,4',
+    'x86_64',
+  ],
   journal: {
     journalUrl: 'https://dev.mes.dev-dvsacloud.uk/v1/journals/{staffNumber}/personal',
     autoRefreshInterval: 20000,
