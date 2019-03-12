0a35f5eb7f3b732fa0c9d0d01c9227b94005dd3b
John Morley <john.morley@bjss.com>
2019-03-12 16:29:44 +0000

    Substitute Lambda environment into endpoint URLs
    * Make config work in envs that aren't dev (e.g. perf)
    * Read environment from envvar, as we have this on all Lambdas in AWS
    * Use sls dotenv plugin to configure the environment locally
    * Make linting rules consistent with the rest of the microservices

--- src/functions/getConfiguration/domain/config.mock.ts
+++ src/functions/getConfiguration/domain/config.mock.ts
@@ -1,4 +1,9 @@
 import { Config } from './config.model';
+import { environment } from '../framework/environment';
+
+const populateEnvironment = (val: string) => {
+  return val.replace('{env}', environment());
+};
 
 export const config : Config = {
   googleAnalyticsId: 'UA-129489007-3',
@@ -7,12 +12,12 @@ export const config : Config = {
     'x86_64',
   ],
   journal: {
-    journalUrl: 'https://dev.mes.dev-dvsacloud.uk/v1/journals/{staffNumber}/personal',
+    journalUrl: populateEnvironment('https://{env}.mes.dev-dvsacloud.uk/v1/journals/{staffNumber}/personal'),
     autoRefreshInterval: 20000,
     numberOfDaysToView: 7,
   },
   logs: {
-    url: 'https://dev.mes.dev-dvsacloud.uk/v1/logs',
+    url: populateEnvironment('https://{env}.mes.dev-dvsacloud.uk/v1/logs'),
     autoSendInterval: 60000,
   },
 };
