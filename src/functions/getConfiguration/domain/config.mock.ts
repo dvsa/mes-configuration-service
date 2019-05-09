ac559a45d522922061c20026a908d42bda33c4e5
Ammar Haider <ammar.haider@bjss.com>
2019-05-09 16:33:21 +0100

    consumes base api url as env var and makes analytics id dynamic

--- src/functions/getConfiguration/domain/config.mock.ts
+++ src/functions/getConfiguration/domain/config.mock.ts
@@ -1,28 +1,33 @@
 import { Config } from './config.model';
 import { environment } from '../framework/environment';
+import { getBaseApiUrl } from '../framework/getBaseApiUrl';
 
-const populateEnvironment = (val: string) => {
-  return val.replace('{env}', environment());
+const generateAnalyticsAccountSuffix = (env: string): string => {
+  return env === Scope.PROD ? '4' : '3';
 };
+
 const generateAllowedTestCategories = (env: string): string[] => {
-  return env === 'dev' ? ['B'] : [];
+  return env === Scope.DEV ? ['B'] : [];
 };
+
+const env = environment();
+const baseApiUrl = getBaseApiUrl();
 
 export const config : Config = {
-  googleAnalyticsId: 'UA-129489007-3',
+  googleAnalyticsId: `UA-129489007-${generateAnalyticsAccountSuffix(env)}`,
   approvedDeviceIdentifiers: [
     'iPad7,4',
     'x86_64',
   ],
   journal: {
-    journalUrl: populateEnvironment('https://{env}.mes.dev-dvsacloud.uk/v1/journals/{staffNumber}/personal'),
+    journalUrl: `${baseApiUrl}/journals/{staffNumber}/personal`,
     autoRefreshInterval: 20000,
     numberOfDaysToView: 7,
     allowTests: true,
-    allowedTestCategories: generateAllowedTestCategories(environment()),
+    allowedTestCategories: generateAllowedTestCategories(env),
   },
   logs: {
-    url: populateEnvironment('https://{env}.mes.dev-dvsacloud.uk/v1/logs'),
+    url: `${baseApiUrl}/logs`,
     autoSendInterval: 60000,
   },
 };
