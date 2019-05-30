977d411cd0137c409d52319d1e7bab9573fc11e0
ammarhaiderbjss <33695049+ammarhaiderbjss@users.noreply.github.com>
2019-05-30 17:26:14 +0100

    adds custom metrics log (#21)

--- src/functions/getConfiguration/framework/handler.ts
+++ src/functions/getConfiguration/framework/handler.ts
@@ -3,6 +3,7 @@ import createResponse from '../../../common/application/utils/createResponse';
 import Response from '../../../common/application/api/Response';
 import { config } from '../domain/config.mock';
 import { Scope } from '../domain/scopes.constants';
+import { customMetric } from '../../../common/application/utils/logger';
 
 export async function handler(event: APIGatewayProxyEvent): Promise<Response> {
 
@@ -11,7 +12,7 @@ export async function handler(event: APIGatewayProxyEvent): Promise<Response> {
   }
 
   const scope: Scope = event.pathParameters.scope as Scope;
-
+  customMetric('ConfigurationReturned', 'Number of times the configuration has been returned to a user');
   return createResponse(config);
 
 }
