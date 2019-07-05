5287694ab39fcadf55d686e576df6adfedbe305e
John Morley <john.morley@bjss.com>
2019-07-05 09:52:58 +0100

    Obtain the staffNumber from the APIG request context

--- src/functions/getConfiguration/framework/__tests__/handler.spec.ts
+++ src/functions/getConfiguration/framework/__tests__/handler.spec.ts
@@ -1,6 +1,6 @@
 import { handler } from '../handler';
 import * as createResponse from '../../../../common/application/utils/createResponse';
-import { APIGatewayEvent } from 'aws-lambda';
+import { APIGatewayEvent, APIGatewayProxyEvent } from 'aws-lambda';
 import { config } from '../../domain/config.mock';
 
 const lambdaTestUtils = require('aws-lambda-test-utils');
@@ -12,11 +12,19 @@ describe('get handler', () => {
 
   beforeEach(() => {
     createResponseSpy = spyOn(createResponse, 'default');
-    dummyApigwEvent = lambdaTestUtils.mockEventCreator.createAPIGatewayEvent({
-      pathParameters: {
-        scope: 'dev',
+    // createAPIGatewayEvent won't include the authorizer requestContext key
+    dummyApigwEvent = {
+      ...lambdaTestUtils.mockEventCreator.createAPIGatewayEvent({
+        pathParameters: {
+          scope: 'dev',
+        },
+      }),
+      requestContext: {
+        authorizer: {
+          staffNumber: '123',
+        },
       },
-    });
+    };
   });
 
   describe('given the handler returns config', () => {
@@ -28,6 +36,18 @@ describe('get handler', () => {
       expect(resp.statusCode).toBe(200);
       expect(createResponse.default)
         .toHaveBeenCalledWith(config);
+    });
+  });
+
+  describe('requestContext handling', () => {
+    it('should return 500 when the requestContext does not have any requestContext', async () => {
+      createResponseSpy.and.returnValue({ statusCode: 500 });
+      const { requestContext, ...eventWithoutRequestContext } = dummyApigwEvent;
+
+      const resp = await handler(eventWithoutRequestContext as APIGatewayProxyEvent);
+
+      expect(resp.statusCode).toBe(500);
+      expect(createResponse.default).toHaveBeenCalledWith('No staffNumber found in request context', 500);
     });
   });
 });
--- src/functions/getConfiguration/framework/handler.ts
+++ src/functions/getConfiguration/framework/handler.ts
@@ -11,8 +11,25 @@ export async function handler(event: APIGatewayProxyEvent): Promise<Response> {
     return createResponse('No Scope Provided', 400);
   }
 
+  if (getStaffNumberFromRequestContext(event) === null) {
+    return createResponse('No staffNumber found in request context', 500);
+  }
+
   const scope: Scope = event.pathParameters.scope as Scope;
   customMetric('ConfigurationReturned', 'Number of times the configuration has been returned to a user');
   return createResponse(config);
 
 }
+
+const getStaffNumberFromRequestContext = (event: APIGatewayProxyEvent): string | null => {
+  if (
+    event
+    && event.requestContext
+    && event.requestContext.authorizer
+    && typeof event.requestContext.authorizer.staffNumber === 'string'
+  ) {
+    console.log(`###################### staffNumber: ${event.requestContext.authorizer.staffNumber}`);
+    return event.requestContext.authorizer.staffNumber;
+  }
+  return null;
+};
