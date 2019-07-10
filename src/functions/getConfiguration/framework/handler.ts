2c11eee53a64c2884f7e83dcbb07a707c77372bf
chris.nappin <chris@nappin.com>
2019-07-10 14:50:18 +0100

    Use common logger, and update vulnerable deps

--- src/functions/getConfiguration/framework/handler.ts
+++ src/functions/getConfiguration/framework/handler.ts
@@ -1,17 +1,20 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import { bootstrapLogging, customMetric, error, info } from '@dvsa/mes-microservice-common/application/utils/logger';
 import createResponse from '../../../common/application/utils/createResponse';
 import Response from '../../../common/application/api/Response';
 import { config } from '../domain/config.mock';
 import { Scope } from '../domain/scopes.constants';
-import { customMetric } from '../../../common/application/utils/logger';
 
 export async function handler(event: APIGatewayProxyEvent): Promise<Response> {
+  bootstrapLogging('configuration-service', event);
 
   if (!event.pathParameters) {
+    error('No scope provided');
     return createResponse('No Scope Provided', 400);
   }
 
   const scope: Scope = event.pathParameters.scope as Scope;
+  info('Returning configuration for ', scope);
   customMetric('ConfigurationReturned', 'Number of times the configuration has been returned to a user');
   return createResponse(config);
 
