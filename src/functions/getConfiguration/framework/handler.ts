3c71eba63eb3cc389c8466bb2eb30b217a096080
matthew <m.bell2412@hotmail.com>
2020-07-16 08:39:03 +0100

    add new examiner role check

--- src/functions/getConfiguration/framework/handler.ts
+++ src/functions/getConfiguration/framework/handler.ts
@@ -1,4 +1,4 @@
-import { APIGatewayProxyEvent, APIGatewayEventRequestContext } from 'aws-lambda';
+import { APIGatewayEventRequestContext, APIGatewayProxyEvent } from 'aws-lambda';
 import { bootstrapLogging, customMetric, error, info } from '@dvsa/mes-microservice-common/application/utils/logger';
 import createResponse from '../../../common/application/utils/createResponse';
 import Response from '../../../common/application/api/Response';
@@ -59,7 +59,13 @@ const getStaffNumberFromRequestContext = (requestContext: APIGatewayEventRequest
 
 const getExaminerRoleFromRequestContext = (requestContext: APIGatewayEventRequestContext): ExaminerRole => {
   if (requestContext.authorizer) {
-    return requestContext.authorizer.examinerRole === 'LDTM' ? ExaminerRole.LDTM : ExaminerRole.DE;
+    const { examinerRole } = requestContext.authorizer;
+
+    switch (examinerRole) {
+      case 'DLG': return ExaminerRole.DLG;
+      case 'LDTM': return ExaminerRole.LDTM;
+      default: return ExaminerRole.DE;
+    }
   }
   // If role is missing we default to DE
   // We only use this for search settings not for authentication so there is no risk giving the user
